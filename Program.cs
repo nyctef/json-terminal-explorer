e4aa1c07a9f6949b10069acbc6900724ed73f296
Nyctef <nyctef@nyctef.com>
2021-10-02 09:49:06 +0100

    initial commit: new console app with Spectre.Console

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace json_terminal_explorer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
